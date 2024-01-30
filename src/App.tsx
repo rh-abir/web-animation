dfc21f3dae70aa1e89eae2670c96f93255971076
rh-abir <rhabir.co@gmail.com>
2024-01-30 16:14:54 +0600

    animation with js

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import "./App.css";
 
 function App() {
+  const name = "Programming Here";
+
+  const nameArrr = name.split("");
+
   return (
     <div className="main">
       {/* <button className="btn">Hove me</button> */}
@@ -11,13 +15,24 @@ function App() {
         <div className="box"></div>
       </div> */}
 
-      <div className="card">
+      {/* <div className="card">
         <h1>Heading</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
           doloremque ut molestiae ex nostrum maxime, quas sapiente velit tenetur
           nisi, nulla saepe accusa
         </p>
+      </div> */}
+
+      <div className="container">
+        {nameArrr.map((item, i) => (
+          <span
+            className="alphabet"
+            style={{ transitionDelay: `${i * 100}ms` }}
+          >
+            {item}
+          </span>
+        ))}
       </div>
     </div>
   );
