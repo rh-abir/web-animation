baa9e91d4a0dd19e44cfef5fd4b12f02f3808620
rh-abir <rhabir.co@gmail.com>
2024-01-30 15:06:35 +0600

    make rotation animation

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ function App() {
   return (
     <div className="main">
       {/* <button className="btn">Hove me</button> */}
-      <div className="box"></div>
+      {/* <div className="box"></div> */}
+
+      <div className="loading">
+        <div className="glass"></div>
+        <div className="box"></div>
+      </div>
     </div>
   );
 }
