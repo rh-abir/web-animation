4d911700c6557f237b123afa5a78d5eed09ce398
rh-abir <rhabir.co@gmail.com>
2024-01-30 14:45:21 +0600

    css animation

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="main">
-      <div className="box-1"></div>
-      <div className="box-container-2">
-        <div className="box-2"></div>
-      </div>
+      {/* <button className="btn">Hove me</button> */}
+      <div className="box"></div>
     </div>
   );
 }
