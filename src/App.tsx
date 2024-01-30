4ebb35d3100979ea4f35804f892d9b70d57a73b3
rh-abir <rhabir.co@gmail.com>
2024-01-30 15:50:57 +0600

    use css clip path

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,19 @@ function App() {
     <div className="main">
       {/* <button className="btn">Hove me</button> */}
       {/* <div className="box"></div> */}
-
+      {/* 
       <div className="loading">
         <div className="glass"></div>
         <div className="box"></div>
+      </div> */}
+
+      <div className="card">
+        <h1>Heading</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+          doloremque ut molestiae ex nostrum maxime, quas sapiente velit tenetur
+          nisi, nulla saepe accusa
+        </p>
       </div>
     </div>
   );
