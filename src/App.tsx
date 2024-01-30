6179c1456bf755dc489075abc3f98c5caf9aa0b9
rh-abir <rhabir.co@gmail.com>
2024-01-30 11:23:15 +0600

    set the animation

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="main">
       <div className="box-1"></div>
+      <div className="box-container-2">
+        <div className="box-2"></div>
+      </div>
     </div>
   );
 }
